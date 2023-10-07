fb50cde5d875112b5adfb3e889477c67b8110610
Benj1x <benjaminh2001@hotmail.dk>
2023-10-08 00:57:09 +0200

    Added DB calls, and sorted them

--- lib/dbConnection/apiConnection.dart
+++ lib/dbConnection/apiConnection.dart
@@ -12,18 +12,3 @@ final pool = MySQLConnectionPool(
   maxConnections: 1,
   databaseName: 'calorieCal', // optional,
 );
-
-//SELECT * FROM calories WHERE userID=SomeID;
-
-//login
-//SELECT * FROM users WHERE users.Email = '" + loginEmail (check if pass matches)
-//Save user ID
-
-//Get recipes
-//SELECT recipes.title, recipes.price, recipes.category FROM recipes;
-
-//Add to calories
-//INSERT INTO Calories VALUES (4, 0, NOW(), 1);
-
-//Add to users
-//INSERT INTO users VALUES (*ID*, username/email, password, caloriegoal);
--- lib/main.dart
+++ lib/main.dart
@@ -68,14 +68,10 @@ class MyAppState extends ChangeNotifier {
     notifyListeners();
   }
 
-  void Login(String username, String password) async {
-    UserID = await TryLogin(username, password);
-    notifyListeners();
-  }
-
-  Future<int> TryLogin(String username, String password) async
-  {
-    Future<int> UserID = Future<int>.value(0);
+  /**
+   * Creates a new users with [email], [username], and a hashed version of [password]
+   */
+  void CreateNewUser(String email, String username, String password) async {
     final conn = await MySQLConnection.createConnection(
       host: '127.0.0.1',
       port: 3306,
@@ -85,23 +81,11 @@ class MyAppState extends ChangeNotifier {
     );
 
     await conn.connect();
+
     var res = await conn.execute(
-        "SELECT * FROM users WHERE Username='" + username + "'", {}, true);
-
-    res.rowsStream.listen((row) {
-      String cRow = row.assoc().toString();
-      final SplitString = cRow.split(", ");
-      final SplitNum = SplitString[0].split(": ");
-      print(SplitNum[1]);
-      var intID = int.parse(SplitNum[1]);
-      UserID = Future<int>.value(intID);
-    });
-
-    conn.close();
-
-    return UserID;
-
-
+        "INSERT INTO users (Email, Username, Password) VALUE ('" + email + "', '" + username + "', MD5('"+ password + "'));");
+    print(res.affectedRows);
+    await conn.close();
   }
 
   void SignUp(String email, String username, String password) async {
@@ -119,7 +103,10 @@ class MyAppState extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<bool> CheckUserExists(String username, String password) async{
+  /**
+   * Verifies that a user has an account with the [email], since the username can be the same for multiple users
+   */
+  Future<bool> CheckUserExists(String email, String password) async{
     Future<int> UserID = Future<int>.value(0);
     final conn = await MySQLConnection.createConnection(
       host: '127.0.0.1',
@@ -131,7 +118,7 @@ class MyAppState extends ChangeNotifier {
 
     await conn.connect();
     var res = await conn.execute(
-        "SELECT * FROM users WHERE Username='" + username + "'", {}, true);
+        "SELECT * FROM users WHERE Email='" + email + "'", {}, true);
     conn.close();
 
     //A very digusting way of returning TRUE if there is a user, and FALSE if there isn't
@@ -142,7 +129,20 @@ class MyAppState extends ChangeNotifier {
     }
   }
 
-  void AddCalories(var CaloriesToAdd) async{
+  /**
+   * Log in, what else is there to say
+   */
+  void Login(String username, String password) async {
+    UserID = await TryLogin(username, password);
+    notifyListeners();
+  }
+
+  /**
+   * Attempts to login the user, on success saves the UserID (Should also save username), and stores it for later use
+   */
+  Future<int> TryLogin(String email, String password) async
+  {
+    Future<int> UserID = Future<int>.value(0);
     final conn = await MySQLConnection.createConnection(
       host: '127.0.0.1',
       port: 3306,
@@ -153,15 +153,67 @@ class MyAppState extends ChangeNotifier {
 
     await conn.connect();
     var res = await conn.execute(
-    "INSERT INTO Calories VALUES (1, "+ CaloriesToAdd +", NOW(), 1);");
+        "SELECT * FROM users WHERE Email='" + email + "' AND Password=MD5('"+password+"')", {}, true);
+
+    res.rowsStream.listen((row) {
+      String cRow = row.assoc().toString();
+      final SplitString = cRow.split(", ");
+      final SplitNum = SplitString[0].split(": ");
+      print(SplitNum[1]);
+      var intID = int.parse(SplitNum[1]);
+      UserID = Future<int>.value(intID);
+    });
+
+    conn.close();
+
+    return UserID;
+  }
+
+  /**
+   * Adds calories for a meal using the [UserID] & [CaloriesToAdd] variables
+   * Function also creates a DateTime.now variable, to use for tracking the time,
+   * this way we can use the users local time, instead of the server local time
+   */
+  void AddCalories(String UserID, String CaloriesToAdd) async{
+    final conn = await MySQLConnection.createConnection(
+      host: '127.0.0.1',
+      port: 3306,
+      userName: 'root',
+      password: '1234',
+      databaseName: 'calorieCal', // optional,
+    );
+    final DTNow = DateTime.now().toString();
+    await conn.connect();
+    var res = await conn.execute(
+    "INSERT INTO Calories (UserID, CaloriesConsumed, Time) VALUES ('"+UserID+"', '"+ CaloriesToAdd +"', "+ DTNow +");");
 
     await conn.close();
   }
 
   /**
-   * Won't work until DB has been updated to use passwords and auto set userIDs (Statement does work though)
+   * Getter function for showing all calorie data for the user
+   * TODO: alternate functions would be [main.GetCaloriesDay] and a delete function
    */
-  void CreateNewUser(String email, String username, String password) async {
+  void GetAllCalories(String UserID) async{
+    final conn = await MySQLConnection.createConnection(
+      host: '127.0.0.1',
+      port: 3306,
+      userName: 'root',
+      password: '1234',
+      databaseName: 'calorieCal', // optional,
+    );
+    final DTNow = DateTime.now().toString();
+    await conn.connect();
+    var res = await conn.execute(
+        "SELECT * FROM Calories WHERE UserID='"+UserID+"');");
+
+    await conn.close();
+  }
+
+  /**
+   * Adds a recipe to the users favorites using the user and recipe ID
+   */
+  void AddRecipeToFavorites(String UserID, String RecipeID) async{
     final conn = await MySQLConnection.createConnection(
       host: '127.0.0.1',
       port: 3306,
@@ -171,10 +223,85 @@ class MyAppState extends ChangeNotifier {
     );
 
     await conn.connect();
-
     var res = await conn.execute(
-        "INSERT INTO users (Email, Username, Password) VALUE ('" + email + "', '" + username + "', MD5('" + password + "'));");
-    print(res.affectedRows);
+        "INSERT INTO favorites (UserID, RecipeID) VALUES ('"+UserID+"', '"+ RecipeID+"');");
+
+    await conn.close();
+  }
+
+  /**
+   * Gets a users favorite recipes to the users favorites using the user and recipe ID
+   */
+  void GetFavorites(String UserID, String RecipeID) async{
+    final conn = await MySQLConnection.createConnection(
+      host: '127.0.0.1',
+      port: 3306,
+      userName: 'root',
+      password: '1234',
+      databaseName: 'calorieCal', // optional,
+    );
+
+    await conn.connect();
+    var res = await conn.execute(
+        "SELECT * FROM favorites WHERE UserID='"+UserID+"');");
+
+    await conn.close();
+  }
+
+  /**
+   * Removes a users favorite recipe
+   */
+  void RemoveFavorites(String UserID, String RecipeID) async{
+    final conn = await MySQLConnection.createConnection(
+      host: '127.0.0.1',
+      port: 3306,
+      userName: 'root',
+      password: '1234',
+      databaseName: 'calorieCal', // optional,
+    );
+
+    await conn.connect();
+    var res = await conn.execute(
+        "DELETE FROM favorites WHERE UserID='"+UserID+"' AND RecipeID='"+RecipeID+"');");
+
+    await conn.close();
+  }
+
+  /**
+   * A general getter function for getting all info
+   */
+  void GetRecipeFromID(String RecipeID) async{
+    final conn = await MySQLConnection.createConnection(
+      host: '127.0.0.1',
+      port: 3306,
+      userName: 'root',
+      password: '1234',
+      databaseName: 'calorieCal', // optional,
+    );
+
+    await conn.connect();
+    var res = await conn.execute(
+        "SELECT * recipes WHERE RecipeID='"+RecipeID+"');");
+
+    await conn.close();
+  }
+
+  /**
+   * A general getter function for getting all info
+   */
+  void GetRecipeFromSkeleton(String RecipeID) async{
+    final conn = await MySQLConnection.createConnection(
+      host: '127.0.0.1',
+      port: 3306,
+      userName: 'root',
+      password: '1234',
+      databaseName: 'calorieCal', // optional,
+    );
+
+    await conn.connect();
+    var res = await conn.execute(
+        "SELECT RecipeTitle, Categories, Description FROM recipes WHERE RecipeID='"+RecipeID+"');");
+
     await conn.close();
   }
 
